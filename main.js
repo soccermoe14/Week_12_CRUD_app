28310432f9c23bce87a38924a06f96f291ec821c
soccermoe14 <114617209+soccermoe14@users.noreply.github.com>
2023-01-13 16:29:33 -0600

    fixed it works

--- main.js
+++ main.js
@@ -31,11 +31,11 @@ function insertNewRecord(data) {
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fullName;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
+    cell2.innerHTML = data.birthdate;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.dessert;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
+    cell4.innerHTML = data.office;
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
